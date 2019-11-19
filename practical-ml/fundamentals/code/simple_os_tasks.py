229e71786b745b20ba40fbf47e46d6f914f6efbd
Wechuli <wechulipaul@yahoo.com>
2019-11-19 08:46:08 +0300

    stratified sampling

--- practical-ml/fundamentals/code/simple_os_tasks.py
+++ practical-ml/fundamentals/code/simple_os_tasks.py
@@ -1,8 +1,19 @@
 import os
-import tarfile
-import urllib.request as request
-import pandas as pd
 import numpy as np
+import pandas as pd
+import urllib.request as request
+import tarfile
+from zlib import crc32
+
+
+def test_set_check(identifier, test_ratio):
+    return crc32(
+        np.int64(identifier)) & 0xffffffff < test_ratio * 2 ** 32
+
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column] in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~ in_test_set], data.loc[in_test_set]
 
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
@@ -19,7 +30,11 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz.close()
 
 
-fetch_housing_data()
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
 
 
-pd.read_
+housing = load_housing_data()
+print(housing.head())
+print(dir(housing))
